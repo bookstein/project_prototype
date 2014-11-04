f3fa6a77b6930081b377c8d705e2820e044969d6
emilyboo <ehbookstein@gmail.com>
2014-11-04 15:14:49 -0800

    Replaced twitter library with tweepy, tested, printed api object
    Former-commit-id: 8ddd9ccde36836c5407359a43f09e6092270f1ca

--- twitter-proof/prototype.py
+++ twitter-proof/prototype.py
@@ -1,4 +1,4 @@
-import twitter
+import tweepy
 import os
 
 TWITTER_API_KEY=os.environ.get('TWITTER_API_KEY')
@@ -11,26 +11,9 @@ TWITTER_SECRET_TOKEN=os.environ.get('TWITTER_SECRET_TOKEN')
 # print TWITTER_ACCESS_TOKEN
 # print TWITTER_API_KEY
 
-auth = twitter.oauth.OAuth(TWITTER_ACCESS_TOKEN, TWITTER_SECRET_TOKEN, TWITTER_API_KEY, TWITTER_SECRET_KEY)
-twitter_api = twitter.Twitter(auth=auth)
+auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_SECRET_KEY)
+auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_SECRET_TOKEN)
 
-# testing out search API
-# url = "https://api.twitter.com/1.1/search/tweets.json?q=voter&src=typd"
+api = tweepy.API(auth)
 
-
-q = 'voter' # query
-count = 5
-results = twitter_api.search.tweets(q=q, count=count)
-statuses = results['statuses']
-print statuses
-
-for _ in range(count):
-	# print statuses
-	try:
-		next_results = results['search_metadata']['next_results']
-
-	# when no more results exist
-	except KeyError, e:
-		break
-
-# print statuses # NOTE: a count of 100 took a second to process.
+print api
