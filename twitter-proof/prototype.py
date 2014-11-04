11276142034b702ffb4c5e8d7024ff2553e0ed68
emilyboo <ehbookstein@gmail.com>
2014-11-04 15:12:53 -0800

    Query Twitter search api using twitter library. Gets results statuses
    Former-commit-id: ba1c1ff6fd70803e49f62da4f68c7b301b06e880

--- twitter-proof/prototype.py
+++ twitter-proof/prototype.py
@@ -6,12 +6,31 @@ TWITTER_SECRET_KEY=os.environ.get('TWITTER_SECRET_KEY')
 TWITTER_ACCESS_TOKEN=os.environ.get('TWITTER_ACCESS_TOKEN')
 TWITTER_SECRET_TOKEN=os.environ.get('TWITTER_SECRET_TOKEN')
 
-print TWITTER_SECRET_TOKEN
-print TWITTER_SECRET_KEY
-print TWITTER_ACCESS_TOKEN
-print TWITTER_API_KEY
+# print TWITTER_SECRET_TOKEN
+# print TWITTER_SECRET_KEY
+# print TWITTER_ACCESS_TOKEN
+# print TWITTER_API_KEY
 
 auth = twitter.oauth.OAuth(TWITTER_ACCESS_TOKEN, TWITTER_SECRET_TOKEN, TWITTER_API_KEY, TWITTER_SECRET_KEY)
 twitter_api = twitter.Twitter(auth=auth)
 
-print twitter_api
+# testing out search API
+# url = "https://api.twitter.com/1.1/search/tweets.json?q=voter&src=typd"
+
+
+q = 'voter' # query
+count = 5
+results = twitter_api.search.tweets(q=q, count=count)
+statuses = results['statuses']
+print statuses
+
+for _ in range(count):
+	# print statuses
+	try:
+		next_results = results['search_metadata']['next_results']
+
+	# when no more results exist
+	except KeyError, e:
+		break
+
+# print statuses # NOTE: a count of 100 took a second to process.
