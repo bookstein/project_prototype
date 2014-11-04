2ee69a5f6f3476fa2938202680407cd3e6f3cfbd
emilyboo <ehbookstein@gmail.com>
2014-11-04 14:38:00 -0800

    Moved all fb prototypes to a dir renamed fb-proof. Created new dir for twitter proof of concept. created initial call to twitter API. Stored API keys in environ vars
    Former-commit-id: 7b0f488e8418c9573e2aa97d5fa3cd57e3b59e49

--- PythonForFbSDK/examples/flask/run.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-from os import environ
-
-from app import app, db
-
-db.create_all()
-app.run(host='0.0.0.0', port=8000)
---
+++ twitter-proof/prototype.py
@@ -0,0 +1,17 @@
+import twitter
+import os
+
+TWITTER_API_KEY=os.environ.get('TWITTER_API_KEY')
+TWITTER_SECRET_KEY=os.environ.get('TWITTER_SECRET_KEY')
+TWITTER_ACCESS_TOKEN=os.environ.get('TWITTER_ACCESS_TOKEN')
+TWITTER_SECRET_TOKEN=os.environ.get('TWITTER_SECRET_TOKEN')
+
+print TWITTER_SECRET_TOKEN
+print TWITTER_SECRET_KEY
+print TWITTER_ACCESS_TOKEN
+print TWITTER_API_KEY
+
+auth = twitter.oauth.OAuth(TWITTER_ACCESS_TOKEN, TWITTER_SECRET_TOKEN, TWITTER_API_KEY, TWITTER_SECRET_KEY)
+twitter_api = twitter.Twitter(auth=auth)
+
+print twitter_api
