e5607dba8808a7968f09ca5c96c9f6b642001fa8
emilyboo <ehbookstein@gmail.com>
2014-11-04 15:53:02 -0800

    Changed link to submit button in form, added app route to handle login POST request
    Former-commit-id: 25ee5aa448b46d6764f6fd380baab8d2b0299ec6

--- twitter-proof/flask_server.py
+++ twitter-proof/flask_server.py
@@ -1,10 +1,24 @@
 from flask import Flask, request, render_template, redirect
+import os
 
 app = Flask(__name__)
+TWITTER_API_KEY=os.environ.get('TWITTER_API_KEY')
+TWITTER_SECRET_KEY=os.environ.get('TWITTER_SECRET_KEY')
+TWITTER_ACCESS_TOKEN=os.environ.get('TWITTER_ACCESS_TOKEN')
+TWITTER_SECRET_TOKEN=os.environ.get('TWITTER_SECRET_TOKEN')
+
 
 @app.route("/")
 def index():
 	return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+	if request.method == "POST":
+		print "posting"
+		return redirect("/")
+	pass
+
 
 
 if __name__ == "__main__":
