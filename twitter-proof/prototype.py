819a99df440eb2c4194cc3d946c441eefa1cbe5a
emilyboo <ehbookstein@gmail.com>
2014-11-04 17:45:16 -0800

    Get timeline by user id. Returns last 20 tweets
    Former-commit-id: e05850b49bd4b5ba3fec76963fd03699129fa5a0

--- twitter-proof/prototype.py
+++ twitter-proof/prototype.py
@@ -16,11 +16,16 @@ auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_SECRET_TOKEN)
 
 api = tweepy.API(auth)
 
-def get_timeline():
-	public_tweets = api.home_timeline() # returns 20 most recent statuses, equivalent to timeline/home
+def get_feed():
+	# returns 20 most recent statuses from authenticated user feed
+	public_tweets = api.home_timeline()
 	for tweet in public_tweets:
-		# print tweet.text
 		pass
+
+def get_timeline(uid):
+	#Returns the 20 most recent statuses posted from the authenticating user
+	tweets = api.user_timeline(uid)
+	return tweets
 
 def get_user_by_id(uid):
 	# get_user can take screen_name or user_id
@@ -33,10 +38,16 @@ def get_friends(uid):
 	friends_ids = api.friends_ids(uid)
 	return friends_ids
 
+def get_links(tweets):
+	url = tweets[0]["expanded_url"]
+	return url
+
 def main():
 	friends = get_friends("bookstein")
-	print friends
-	print get_user_by_id(friends[0]).screen_name
+	friend_id = get_user_by_id(friends[0]).id
+	print friend_id
+	tweets = get_timeline(friend_id)
+	print tweets
 
 if __name__ == "__main__":
 	main()
