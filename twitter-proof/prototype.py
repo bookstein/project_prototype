12be94f36caeb752458e326f3e7ae0defc0e883d
emilyboo <ehbookstein@gmail.com>
2014-11-05 13:12:58 -0800

    Html form asks for twitter username. New python functions assign random scores to user friends.
    Score is currently a random number
    Data stored in dictionary
    All user friends assigned scores, stored as dict value
    
    
    Former-commit-id: cfa4b6d09fcf3baef611abbfae007a506afc0df1

--- twitter-proof/prototype.py
+++ twitter-proof/prototype.py
@@ -1,16 +1,12 @@
 import tweepy
 import os
 import time
+from random import randint
 
 TWITTER_API_KEY=os.environ.get('TWITTER_API_KEY')
 TWITTER_SECRET_KEY=os.environ.get('TWITTER_SECRET_KEY')
 TWITTER_ACCESS_TOKEN=os.environ.get('TWITTER_ACCESS_TOKEN')
 TWITTER_SECRET_TOKEN=os.environ.get('TWITTER_SECRET_TOKEN')
-
-# print TWITTER_SECRET_TOKEN
-# print TWITTER_SECRET_KEY
-# print TWITTER_ACCESS_TOKEN
-# print TWITTER_API_KEY
 
 auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_SECRET_KEY)
 auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_SECRET_TOKEN)
@@ -49,18 +45,31 @@ def get_links(id):
 		# link_dictionary.setdefault(key, tweet["entities"]["urls"])
 	return link_dictionary
 
+def assign_score():
+	score = randint(0, 100)
+	return score
+
+def assign_all_friend_scores(friendlist):
+	pass
+	scores = {}
+	for friend_id in friendlist:
+		scores.setdefault(friend_id, assign_score())
+	return scores
+
 def timer():
 	time.sleep(900)
 	print "It's been 900 seconds"
 
+
 def main():
 	# timer()
 	friends = get_friends("bookstein")
-	friend_id = get_user_by_id(friends[0]).id
+	# friend_id = get_user_by_id(friends[0]).id
 	# print friend_id
 	# tweets = get_timeline(friend_id)
 	# print tweets
-	print get_links(friend_id)
+	# print get_links(friend_id)
+	print assign_all_friend_scores(friends)
 
 
 if __name__ == "__main__":
