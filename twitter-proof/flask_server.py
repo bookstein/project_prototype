f5163b16ca5499d82f4270f2ba8796c04ebff806
emilyboo <ehbookstein@gmail.com>
2014-11-05 13:32:55 -0800

    Debugging prototype.get_friends
    Former-commit-id: 8010425c400912fb1dc99e1cb44393362b4a8e07

--- twitter-proof/flask_server.py
+++ twitter-proof/flask_server.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, render_template, redirect
 import os
+import prototype
 
 app = Flask(__name__)
-TWITTER_API_KEY=os.environ.get('TWITTER_API_KEY')
-TWITTER_SECRET_KEY=os.environ.get('TWITTER_SECRET_KEY')
-TWITTER_ACCESS_TOKEN=os.environ.get('TWITTER_ACCESS_TOKEN')
-TWITTER_SECRET_TOKEN=os.environ.get('TWITTER_SECRET_TOKEN')
+# TWITTER_API_KEY=os.environ.get('TWITTER_API_KEY')
+# TWITTER_SECRET_KEY=os.environ.get('TWITTER_SECRET_KEY')
+# TWITTER_ACCESS_TOKEN=os.environ.get('TWITTER_ACCESS_TOKEN')
+# TWITTER_SECRET_TOKEN=os.environ.get('TWITTER_SECRET_TOKEN')
 
 
 @app.route("/")
@@ -16,6 +17,14 @@ def index():
 def login():
 	if request.method == "POST":
 		print "posting"
+		screen_name = request.form.get("screenname")
+		print screen_name
+		friends = prototype.get_friends(screen_name)
+		print friends #errors!
+		# this worked in python interpreter: flask_server.prototype.get_friends("bookstein")
+
+		# scored_friends = prototype.assign_all_friend_scores(friends)
+		# print scored_friends
 		return redirect("/")
 	pass
 
