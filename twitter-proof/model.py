8eaa2a5f26df5574b0abde4f6325b5dc9927d72c
emilyboo <ehbookstein@gmail.com>
2014-11-05 13:10:45 -0800

    Created class declarations for database structure in SQLAlchemy.
     - User class makes user table
     - Relationship class makes table of relationships between users, with nullable field for mutual friendship
     - Status class makes table to hold statuses of users (tweets). Makes relationship between user and statuses.
    
    
    Former-commit-id: 6def53dcda5fc31769fab1de3a2edafe7c7129ae

---
+++ twitter-proof/model.py
@@ -0,0 +1,50 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import backref, relationship
+from sqlalchemy import ForeignKey
+from sqlalchemy import UniqueConstraint #have these on same line?
+
+ENGINE = create_engine("sqlite:///data.db", echo=False)
+db_session = scoped_session(sessionmaker(bind=ENGINE, autocommit=False, autoflush = False))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+######################
+# class declarations #
+######################
+
+class User(Base):
+	__tablename__ = "users"
+
+	id = Column(Integer, primary_key=True) #same as twitter id?
+	screen_name = Column(String(20), unique=True, nullable=False)
+	score = Column(Integer)
+	location = Column(String(60))
+
+class Relationship(Base):
+	__tablename__ = "relationships"
+
+	id = Column(Integer, primary_key=True) # relationship id
+	user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+	friend_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+	follow_back = Column(Boolean)
+
+class Status(Base):
+	__tablename__ = "statuses"
+
+	id = Column(Integer, primary_key=True) #same as twitter status id?
+	user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+	content = Column(String(140), nullable=False) # tweet can't be empty
+	# etc etc
+
+	#creates "statuses" attribute of user
+	user = relationship("User", backref = backref("statuses"), order_by=id)
+
+def main():
+	pass
+
+if __name__ == "__main__":
+    main()
