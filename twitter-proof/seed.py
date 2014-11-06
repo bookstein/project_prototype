77fd9842d6f8b34cdf5629178fa6d7c7ee504981
emilyboo <ehbookstein@gmail.com>
2014-11-05 17:59:58 -0800

    Wrote script to make .json file for last 20 tweets from each username in USERS var. Got last 20 tweets from celebrity timelines.
    Former-commit-id: 495db4df4d426db8b0e053cc9995d997e148b15c

---
+++ twitter-proof/seed.py
@@ -0,0 +1,22 @@
+# import model
+import prototype
+import json
+
+USERS = ["bookstein", "maddow", "rushlimbaugh", "MatthewKeysLive", "iamjohnoliver",
+"SenRandPaul"]
+
+def make_feed_file(username):
+	prototype.init_api()
+	outfile = open("tweetfiles/"+username+".json", "a+")
+	feed = prototype.get_timeline(username)
+	for status in feed:
+		outfile.write(json.dumps(status._json))
+	outfile.close()
+	# return feed
+
+def main():
+	for name in USERS:
+		make_feed_file(name)
+
+if __name__ == "__main__":
+	main()
