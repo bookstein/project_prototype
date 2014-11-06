41fd138a6ce510d59bced674460878eac67f93e0
emilyboo <ehbookstein@gmail.com>
2014-11-05 16:29:18 -0800

    Add logging functionality to flask, clean up flask code
    import logging, set basicConfig.
    
    
    Former-commit-id: 93530ea25a5b9d59e838257f7587ed01230efec9

--- twitter-proof/flask_server.py
+++ twitter-proof/flask_server.py
@@ -1,12 +1,11 @@
 from flask import Flask, request, render_template, redirect
 import os
 import prototype
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
-# TWITTER_API_KEY=os.environ.get('TWITTER_API_KEY')
-# TWITTER_SECRET_KEY=os.environ.get('TWITTER_SECRET_KEY')
-# TWITTER_ACCESS_TOKEN=os.environ.get('TWITTER_ACCESS_TOKEN')
-# TWITTER_SECRET_TOKEN=os.environ.get('TWITTER_SECRET_TOKEN')
 
 
 @app.route("/")
@@ -22,8 +21,8 @@ def login():
 		# me = prototype.get_user_by_id(screen_name)
 		# print me
 		friends = prototype.get_friends(screen_name)
-		print friends #errors!
-		# this worked in python interpreter: flask_server.prototype.get_friends("bookstein")
+		print friends
+		#error: this worked in python interpreter: flask_server.prototype.get_friends("bookstein")
 
 		# scored_friends = prototype.assign_all_friend_scores(friends)
 		# print scored_friends
