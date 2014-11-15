d3297d09f395424882abf2c700e43fb345334063
emilyboo <ehbookstein@gmail.com>
2014-11-15 12:15:03 -0800

    Nuked .git file from Project on master branch. Re-adding top level files now to a new local .git dir.
    Not yet adding twitter_proof due to large files contained. Will add in separate commit.

---
+++ friends.py
@@ -0,0 +1,124 @@
+"""
+	API for getting friends and statuses from Twitter, scoring.
+"""
+import os
+import pickle
+import tweepy
+import score
+
+
+class User(object):
+
+	# initialize tweepy api object with auth, OAuth
+	TWITTER_API_KEY=os.environ.get('TWITTER_API_KEY')
+	TWITTER_SECRET_KEY=os.environ.get('TWITTER_SECRET_KEY')
+	TWITTER_ACCESS_TOKEN=os.environ.get('TWITTER_ACCESS_TOKEN')
+	TWITTER_SECRET_TOKEN=os.environ.get('TWITTER_SECRET_TOKEN')
+
+	# important variables
+	MAX_NUM_TWEETS = 100
+	MAX_NUM_FRIENDS = 300
+	TIME_TO_WAIT = 900/180 # 15 minutes divided into 180 requests
+	NUM_RETRIES = 2
+	RATE_LIMITED_RESOURCES =[("statuses", "/statuses/user_timeline")]
+
+	CURRENT_USER = ""
+
+	# tweepy api instance
+	api = None
+
+	def __init__(self):
+		"""
+		Create instance of tweepy API class with OAuth keys and tokens.
+		"""
+		auth = tweepy.OAuthHandler(self.TWITTER_API_KEY, self.TWITTER_SECRET_KEY, secure=True)
+		auth.set_access_token(self.TWITTER_ACCESS_TOKEN, self.TWITTER_SECRET_TOKEN)
+		self.api = tweepy.API(auth, cache=None) #removed wait_on_rate_limit=True, wait_on_rate_limit_notify=True
+
+	# def paginate(iterable, page_size):
+
+	def get_friend_ids(self,user_id):
+		"""returns list of integers (friends_ids) of people that user (uid) is following"""
+		try:
+			friends_ids = tweepy.Cursor(self.api.friends_ids, user_id = user_id).pages()
+			# print friends_ids
+			return friends_ids
+		except tweepy.TweepError as e:
+			print e
+
+	def hydrate_friends(self,ids):
+		# if len(ids) > 100:
+		try:
+			friends = self.api.lookup_users(ids)
+			print friends
+		except tweepy.TweepError as e:
+			print e
+
+		# use output of get_friend_ids
+		# hydrate (create twitter user object)
+		# pickle dictionary
+
+	def get_top_influencers(self):
+		pass
+		# friends_count attribute
+		# do this if user has more than 300 friends (180 friends w user auth)
+
+	def get_timeline(self, uid, count):
+		"""Get n number of tweets by passing in user id and number of statuses.
+			If user has protected tweets, returns [] rather than break the program.
+		"""
+		try:
+			feed = tweepy.Cursor(self.api.user_timeline, id=uid).items(count)
+			return feed
+
+		except tweepy.TweepError as e:
+			print e.message[0]["error"]
+			return []
+
+		# try:
+		# except:
+		# get user's timeline
+		# statuses/user_timeline
+		# create Status object
+		# pickle dictionary
+
+	def score_user(self):
+		pass
+		# score user by tweets
+		# store (in DB???)
+
+
+	def get_links(self):
+		pass
+		# get link url, cut to hostname, compare against database
+
+	def score_status(self):
+		pass
+		#score tweet based on links, possibly keywords/hashtags
+		# use score module
+
+
+	def on_error(self):
+		pass
+		# handle errors here
+
+	def check_rate_limit(self):
+		pass
+		# check rate limit for a given resource instead of hardcoding
+
+
+
+def main():
+	pass
+	# currentUser = User(currentId)
+	# currentUser.get_friends()
+	# currentUser.get_timeline()
+
+if __name__ == "__main__":
+
+	# __init__ returns copy of Friends class, including api instance
+	user = User()
+
+	main()
+
+
---
+++ score.py
@@ -0,0 +1,152 @@
+"""
+	Score tweet using this algorithm.
+	A user's score is an aggregate score based on individual tweet scores.
+	Compare link to entry in database, throw out if no corresponding data.
+"""
+import simplejson as json
+import numpy as np
+import os
+import re
+from nltk import NaiveBayesClassifier
+import nltk.classify
+from nltk import FreqDist
+from nltk.tokenize import wordpunct_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from collections import defaultdict
+from string import punctuation
+
+def get_json_data(filename):
+	"""
+	Input filename containing array of JSON-format statuses.
+	Output is list of dictionaries.
+	"""
+	f = open(filename)
+
+	# decodes all file data from json
+	statuses_data = json.load(f)
+	return statuses_data
+
+def parse_statuses(statuses):
+	"""Given decoded json statuses for a given user, gets text, hashtags, and links from all statuses.
+
+	Parameters:
+	----------
+	Output from get_json_data
+
+	Output:
+	-------
+	Arrays containing tweet text, hashtags, urls as strings.
+	"""
+	text_list = []
+	hashtags_list = []
+	urls_list = []
+
+	for status in statuses:
+
+		text = status["text"]
+		entities = status["entities"]
+
+		for i in range(len(entities["urls"])):
+			# appends url as it exists in the tweet text
+			url = entities["urls"][i]["url"]
+			urls_list.append(url)
+			match = re.search(url, text)
+			if match:
+				text = re.sub(r"(?:\@|https?\://)\S+", " ", text)
+				print "TEXT AFTER MATCH ", text
+
+		text_list.append(text)
+		hashtags_list.append(entities["hashtags"])
+
+
+	return text_list, hashtags_list, urls_list
+
+
+def word_count(status_text, affiliation="liberal"):
+	"""
+	Iterates through input user statuses (twitter feed) to calculate word counts.
+
+	Parameters:
+	-----------
+	Text of a given user's statuses, as a list of strings.
+	Political affiliation of user (either liberal or conservative)
+
+
+	Outputs:
+	--------
+	Dictionary with word counts (lowercase word, frequency).
+	This can be used to get scores of a seed group of known political agents.
+
+
+	"""
+	keywords = {
+		"liberal" : {},
+		"conservative" : {}
+	}
+
+	hashtags = {
+		"liberal": {},
+		"conservative" : {}
+	}
+
+	d = keywords[affiliation]
+	h = hashtags[affiliation]
+
+	for text in status_text:
+		text = set(word_tokenize(text.lower()))
+		print text
+
+
+
+	# 	for word in words:
+	# 		if word[0] == "#":
+	# 			hashtag = word[1:]
+	# 			print hashtag
+	# 			h[hashtag] = h.get(hashtag, 0) + 1
+	# 		else:
+	# 			d[word] = d.get(word, 0) + 1
+
+	# print h
+	# return d, h
+
+
+def get_words(text, stopwords = []):
+	"""
+	Given words:
+	Remove punctuation and words smaller than 2 letters, make all words lowercase.
+
+	Inputs:
+	-------
+	Text from all statuses. (?)
+
+	Outputs:
+	--------
+	Text made lowercase, without punctuation, ready to split into tokens.
+	"""
+	for single_tweet in text:
+		print "SINGLE TWEET ", single_tweet
+
+		# tokenize a string to split off punctuation other than periods
+		text_words = set(wordpunct_tokenize(single_tweet))
+		print "WORDPUNCT WORDS", text_words
+
+		# Remove stopwords
+		text_words = text_words.difference(stopwords)
+		print "AFTER REMOVING STOPWORDS", text_words, "\n\n"
+
+	# return text_words
+
+
+def main():
+	# pass
+	data = get_json_data("./json/maddow/self.json")
+	text, hashtags, urls = parse_statuses(data)
+	# processed_text = process_text(text, stopwords)
+	# print processed_text
+	# print get_words(text)
+
+
+if __name__ == "__main__":
+	main()
+
+
