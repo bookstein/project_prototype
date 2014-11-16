647ba154821fe2938e9aecea34ca5f95ab10d507
emilyboo <ehbookstein@gmail.com>
2014-11-16 08:20:24 -0800

    Updated flask_server and index.html for current filenames and routes.
    Flask_server creates a user object from friends.User class to initiate api connection.
    index.html submits form to /display.
    Flask_server returns tweepy cursor object of friends ids (as test).
    Included bootstrap css from static directory.

--- flask_server.py
+++ flask_server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect
 import os
-import tw_api
+import friends
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
@@ -12,7 +12,7 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/display", methods=["GET", "POST"])
 def login():
 	if request.method == "POST":
 		print "posting"
@@ -20,14 +20,15 @@ def login():
 		print screen_name
 		# me = tw_api.get_user_by_id(screen_name)
 		# print me
-		tw_api.init_api()
-		friends = tw_api.get_friends(screen_name)
-		print friends
+		user = friends.User()
+		user.CURRENT_USER = screen_name
+		friendlist = user.get_friend_ids(screen_name)
+		print friendlist
 		#error: this worked in python interpreter: flask_server.tw_api.get_friends("bookstein")
 
 		# scored_friends = tw_api.assign_all_friend_scores(friends)
 		# print scored_friends
-		return render_template("index.html", friends = friends)
+		return render_template("index.html", friends = friendlist)
 	pass
 
 
