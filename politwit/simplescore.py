53fa9ba196dd4d8d7c699f76ef46e834d7396351
emilyboo <ehbookstein@gmail.com>
2014-11-22 10:05:50 -0800

    Rewrite simplescore module as a class, Score, that runs scoring functions on init.
    Init Score from friends.py, score = simplescore.Score().
    Final score is attribute of Score class
    Rewrote as class so that I could run all functions when creating a single score simply by init
    Also because Score will be called for every user and user friend

--- politwit/simplescore.py
+++ politwit/simplescore.py
@@ -2,45 +2,56 @@
 
 import model
 
-def matching_hashtags(hashtags_dict):
-	"""
-	Create dictionary of all political hashtags, incrementing count by user's
-	political hashtags.
+class Score(object):
 
-	Parameters:
-	-----------
-	Hashtags_dict is a dictionary of all user's hashtags. Hashtag value is count.
-
-	Output:
-	-------
-	Dictionary of all political hashtags, where all non-used hashtags have value 0
-	and political hashtags used by user show the number of times used.
-	"""
+	hashtags_dict = {}
 	matches = {}
+	score = 0
 
-	political_hashtags = model.Hashtag.get_all_political_hashtags()
+	def __init__(self, hashtags_dict):
+		self.hashtags_dict = hashtags_dict
+		self.matches = self.matching_hashtags()
+		self.score = self.score_by_hashtags(self.matches)
 
-	for hashtag in political_hashtags:
-		matches[hashtag] = hashtags_dict.get(hashtag, 0)
+	def matching_hashtags(self):
+		"""
+		Create dictionary of all political hashtags, incrementing count by user's
+		political hashtags.
 
-	return matches
+		Parameters:
+		-----------
+		Hashtags_dict is a dictionary of all user's hashtags. Hashtag value is count.
 
-def score_by_hashtags(matches, hashtags_dict):
-	"""
-	Calculate score given matching hashtags.
-	"""
-	political_count = 0
-	total_num_hashtags = 0
+		Output:
+		-------
+		Dictionary of all political hashtags, where all non-used hashtags have value 0
+		and political hashtags used by user show the number of times used.
+		"""
+		matches = {}
 
-	for hashtag in hashtags_dict:
-		total_num_hashtags += hashtags_dict[hashtag]
+		political_hashtags = model.Hashtag.get_all_political_hashtags()
 
-	for hashtag in matches:
-		political_count += matches[hashtag]
+		for hashtag in political_hashtags:
+			matches[hashtag] = self.hashtags_dict.get(hashtag, 0)
 
-	score = float(political_count)/float(total_num_hashtags)
+		return matches
 
-	return score
+	def score_by_hashtags(self, matches):
+		"""
+		Calculate score given matching hashtags.
+		"""
+		political_count = 0
+		total_num_hashtags = 0
+
+		for hashtag in self.hashtags_dict:
+			total_num_hashtags += self.hashtags_dict[hashtag]
+
+		for hashtag in matches:
+			political_count += matches[hashtag]
+
+		score = float(political_count)/float(total_num_hashtags)
+
+		return score
 
 
 def main():
