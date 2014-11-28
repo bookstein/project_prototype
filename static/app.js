44ebbf41813b0bc8a873839c8c819dedfbff980c
emilyboo <ehbookstein@gmail.com>
2014-11-27 16:18:27 -0800

    Remove #panel div from index.html, insert into div (detail-container) only on success in displaySuccess callback.
     - Only will show side panel on successful display of friends.

--- static/app.js
+++ static/app.js
@@ -6,18 +6,23 @@
         if (screen_name.length > 0) {
             console.log(screen_name);
             $("#messages").append("<div class='progress'><span class='meter'></span></div>");
+
             function displaySuccess(response) {
+                console.log("display success");
+                $("#detail-container").append("<div class='panel' id='detail'><h5> Click the bubbles to see more about your Twitter friends.</h5><ul></ul><div id='tweets'></div></div>");
                 VIZ.createVisualization(response);
             }
 
             TwitterAjax.callDisplay(screen_name, displaySuccess);
+
         }
         else {
             $("#result").text("Please enter a valid Twitter handle!");
         }
     });
 
-
+    var progressBar = $('.progress');               // Variable to cache progress bar element
+    var progressBarMeter = $('.progress .meter');   // Variable to cache meter element
 
 
 })();
