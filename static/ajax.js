750ad786e797f32a1b703fd5fec49fb73b3ec4a8
emilyboo <ehbookstein@gmail.com>
2014-12-01 22:34:29 -0800

    Add comments and annotations, delete defunct AJAX call to get tweets (using Twitter timeline widget instead).

--- static/ajax.js
+++ static/ajax.js
@@ -1,33 +1,41 @@
+/*
+  My AJAX library.
+  Adding addition AJAX calls is easy to do and to maintain -- simply add to the returned object.
+*/
 var TwitterAjax = TwitterAjax || (function() {
 
-    function ajaxFunc (url) {
-        // returns a function that calls my Flask server at given url
-        return (function(screen_name, successCallback) {
-            $.ajax({
-                url: url,
-                data: JSON.stringify({"screen_name":screen_name}),
-                type: "POST",
-                dataType: 'json',
-                contentType: 'application/json;charset=UTF-8',
-                success: function(response) {
-                    successCallback(response);
-                },
-                error: function(error) {
-                    console.log(error);
-                }
-            });
+  /*
+    createAjax builds AJAX requests given a url.
+
+    @params: url that corresponds to a Flask route
+    @returns: a function that takes 2 parameters (screen name, success callback) and outputs the success callback's return value.
+  */
+  function createAjax (url) {
+
+    // returns a function that calls my Flask server at a given url
+    return (function(screen_name, successCallback) {
+        $.ajax({
+            url: url,
+            data: JSON.stringify({"screen_name":screen_name}),
+            type: "POST",
+            dataType: 'json',
+            contentType: 'application/json;charset=UTF-8',
+            success: function(response) {
+                successCallback(response);
+            },
+            error: function(error) {
+                console.log(error);
+            }
         });
-    }
+    });
+  }
 
-    return {
+  return {
 
-    // callDisplay is a function that takes a success callback
-        callDisplay: ajaxFunc("/ajax/user"),
+    // callDisplay creates the AJAX request that initiates getting visualization in app.js
+    callDisplay: createAjax("/ajax/user")
 
-        getTweets: ajaxFunc("/ajax/tweets")
-
-    }
-
+  }
 
 })();
 
