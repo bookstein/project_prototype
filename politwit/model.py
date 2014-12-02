59d7820f33f7635e3ff642fd00b404535fb36b31
emilyboo <ehbookstein@gmail.com>
2014-12-01 21:36:06 -0800

    Revise model.py to match PEP8 standards, changing indentation, breaking up lines that are too long.

--- politwit/model.py
+++ politwit/model.py
@@ -1,13 +1,13 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, Integer, String, Date, Boolean
+from sqlalchemy import Table, Column, Integer, String, Date
 from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy.orm import backref, relationship
+from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey
-from sqlalchemy import UniqueConstraint #have these on same line?
 
 ENGINE = create_engine("sqlite:///../tweets_58_42.db", echo=False)
-db_session = scoped_session(sessionmaker(bind=ENGINE, autocommit=False, autoflush = False))
+db_session = scoped_session(sessionmaker(bind=ENGINE, autocommit=False,
+                            autoflush=False))
 
 Base = declarative_base()
 Base.query = db_session.query_property()
@@ -25,94 +25,93 @@ Base.query = db_session.query_property()
 # class declarations #
 ######################
 
-class User(Base):
-	__tablename__ = "users"
 
-	id = Column(Integer, primary_key=True)
-	tw_user_id = Column(Integer, nullable=False)
-	screen_name = Column(String(20), unique=True, nullable=False)
-	num_followers = Column(Integer)
-	num_friends = Column(Integer)
-	score = Column(Integer, nullable=True)
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    tw_user_id = Column(Integer, nullable=False)
+    screen_name = Column(String(20), unique=True, nullable=False)
+    num_followers = Column(Integer)
+    num_friends = Column(Integer)
+    score = Column(Integer, nullable=True)
 
 statuses_hashtags = Table('statuses_hashtags', Base.metadata,
-	Column('id', Integer, primary_key=True),
-    Column('status_id', Integer, ForeignKey('statuses.tw_tweet_id')),#, primary_key=False),
-    Column('hashtag_id', Integer, ForeignKey('hashtags.id')) #, primary_key=True)
-)
+                          Column('id', Integer, primary_key=True),
+                          Column('status_id', Integer,
+                                 ForeignKey('statuses.tw_tweet_id')),
+                          Column('hashtag_id', Integer,
+                                 ForeignKey('hashtags.id'))
+                          )
+
 
 class Status(Base):
-	__tablename__ = "statuses"
+    __tablename__ = "statuses"
 
-	id = Column(Integer, primary_key=True)
-	user_id = Column(Integer, ForeignKey("users.id"), nullable=True)
-	# tw_tweet_id is Twitter API id
-	tw_tweet_id = Column(Integer, nullable=False, unique=True)
-	tw_user_id = Column(Integer, nullable=False)
-	text = Column(String(140), nullable=False)
-	url = Column(String(140), nullable=True)
-	retweeted_from = Column(Integer, nullable=True)
-	created_at = Column(Date)
-	label = Column(String(5), nullable=False)
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=True)
+    # tw_tweet_id is Twitter API id
+    tw_tweet_id = Column(Integer, nullable=False, unique=True)
+    tw_user_id = Column(Integer, nullable=False)
+    text = Column(String(140), nullable=False)
+    url = Column(String(140), nullable=True)
+    retweeted_from = Column(Integer, nullable=True)
+    created_at = Column(Date)
+    label = Column(String(5), nullable=False)
 
-	#creates "statuses" attribute of user
-	user = relationship("User", backref = "statuses")
-	hashtags = relationship("Hashtag", secondary=statuses_hashtags, backref="statuses")
+    #creates "statuses" attribute of user
+    user = relationship("User", backref="statuses")
+    hashtags = relationship("Hashtag", secondary=statuses_hashtags,
+                            backref="statuses")
 
+    @classmethod
+    def get_all_statuses(cls):
+        """
+        Return all rows from statuses table.
 
-	@classmethod
-	def get_all_statuses(cls):
-		"""
-		Return all rows from statuses table.
+        Parameters:
+        -----------
+        'cls' references the Status class.
 
-		Parameters:
-		-----------
-		'cls' references the Status class.
+        Output:
+        ------
+        List of status objects
+        """
+        statuses = cls.query.all()
+        return statuses
 
-		Output:
-		------
-		List of status objects
-		"""
-		statuses = cls.query.all()
-		return statuses
+    @classmethod
+    def get_cons_statuses(cls):
+        """Return all conservative statuses
 
-	@classmethod
-	def get_cons_statuses(cls):
-		"""Return all conservative statuses
+        Parameters:
+        -----------
+        'cls' references the Status class.
 
-		Parameters:
-		-----------
-		'cls' references the Status class.
+        Output:
+        ------
+        List of status objects
+        """
+        statuses = cls.query.filter_by(label="cons").all()
+        return statuses
 
-		Output:
-		------
-		List of status objects
-		"""
-		statuses = cls.query.filter_by(label="cons").all()
-		return statuses
 
 class Hashtag(Base):
-	__tablename__ = "hashtags"
+    __tablename__ = "hashtags"
 
-	id = Column(Integer, primary_key=True)
-	text = Column(String(60))
+    id = Column(Integer, primary_key=True)
+    text = Column(String(60))
 
+    @classmethod
+    def get_all_political_hashtags(cls):
+        """
+        Return all political hashtags from database as dictionary.
 
-	@classmethod
-	def get_co_occurrences(cls, hashtag):
-		"""find hashtags that co-occur in a given tweet"""
-		pass
-		# select tw_tweet_id from statuses inner join hashtags ON (hashtags.status_id = statuses.tw_tweet_id) WHERE (hashtags.text = "tcot") limit(10);
+        """
+        hashtags = cls.query.all()
+        hashtags_text = {tag.text: 1 for tag in hashtags}
+        return hashtags_text
 
-	@classmethod
-	def get_all_political_hashtags(cls):
-		"""
-		Return all political hashtags from database as dictionary.
-
-		"""
-		hashtags = cls.query.all()
-		hashtags_text = {tag.text: 1 for tag in hashtags}
-		return hashtags_text
 
 def connect():
     global ENGINE
